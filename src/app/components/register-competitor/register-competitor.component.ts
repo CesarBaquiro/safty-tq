0115f1e04ea8feb206085e612d7373360c2d803e
CesarBaquiro <cmbm.kf@gmail.com>
2024-06-25 02:21:14 -0500

    Add vehicle form

--- src/app/components/register-competitor/register-competitor.component.ts
+++ src/app/components/register-competitor/register-competitor.component.ts
@@ -17,64 +17,117 @@ import { Observable } from 'rxjs';
   imports: [CommonModule, ReactiveFormsModule],
 })
 export class RegisterCompetitorComponent {
-  form: FormGroup;
+  formCompetitor: FormGroup;
+  formVehicle: FormGroup;
+  showForm: boolean = false;
+  competitor = {
+    allergies: [],
+    contacts: [],
+    vehicle: {},
+  };
+
   uploadProgress$!: Observable<number>;
   downloadURL$!: Observable<string>;
   bloodTypes: string[] = ['O+', 'O-', 'A+', 'A-', 'B+', 'B-', 'AB+', 'AB-'];
-
+  allRisk: boolean = false;
   private fb: FormBuilder = inject(FormBuilder);
   private imagesService: ImagesService = inject(ImagesService);
-  private selectedFile: File | null = null;
+  //private selectedFile: File | null = null;
+  private selectedCompetitorFile: File | null = null;
+  private selectedVehicleFile: File | null = null;
 
   constructor() {
-    this.form = this.fb.group({
+    this.formCompetitor = this.fb.group({
       competitorNum: [
         '',
         [Validators.required, Validators.minLength(3), Validators.maxLength(3)],
       ],
       name: ['', [Validators.required, Validators.minLength(3)]],
       lastname: ['', [Validators.required, Validators.minLength(3)]],
-      bloodType: ['', Validators.required],
+      rh: ['', Validators.required],
       eps: ['', [Validators.required, Validators.minLength(3)]],
       //La imagen no es requerida obligatoriamente
-      image: [null],
+      competitorImage: [null],
+    });
+
+    this.formVehicle = this.fb.group({
+      vehicleReference: ['', [Validators.required, Validators.minLength(3)]],
+      plate: ['', [Validators.maxLength(6)]],
+      numberPolicySoat: [],
+      certificateNumberRTM: [],
+      allRisk: this.allRisk,
+      //La imagen no es requerida obligatoriamente
+      vehicleImage: [null],
     });
   }
 
+  // gets formulario del competidor
+
   get competitorNum() {
-    return this.form.get('competitorNum');
+    return this.formCompetitor.get('competitorNum');
   }
 
   get name() {
-    return this.form.get('name');
+    return this.formCompetitor.get('name');
   }
 
   get lastname() {
-    return this.form.get('lastname');
+    return this.formCompetitor.get('lastname');
   }
 
   get email() {
-    return this.form.get('email');
+    return this.formCompetitor.get('email');
   }
 
-  get bloodType() {
-    return this.form.get('bloodType');
+  get rh() {
+    return this.formCompetitor.get('rh');
   }
 
   get eps() {
-    return this.form.get('eps');
+    return this.formCompetitor.get('eps');
   }
 
-  get image() {
-    return this.form.get('image');
+  get imageCompetitor() {
+    return this.formCompetitor.get('imageCompetitor');
   }
 
-  onFileSelected(event: any) {
+  // gets formulario del vehiculo
+
+  get vehicleReference() {
+    return this.formVehicle.get('vehicleReference');
+  }
+
+  get plate() {
+    return this.formVehicle.get('plate');
+  }
+
+  get numberPolicySoat() {
+    return this.formVehicle.get('numberPolicySoat');
+  }
+
+  get certificateNumberRTM() {
+    return this.formVehicle.get('certificateNumberRTM');
+  }
+
+  get imageVehicle() {
+    return this.formVehicle.get('imageVehicle');
+  }
+
+  onFileSelected(event: any, type: string) {
     const file: File = event.target.files[0];
     if (file) {
-      this.selectedFile = file;
-      this.image?.setValue(file.name);
+      if (type === 'competitor') {
+        this.selectedCompetitorFile = file;
+        this.imageCompetitor?.setValue(file.name);
+      } else if (type === 'vehicle') {
+        this.selectedVehicleFile = file;
+        this.imageVehicle?.setValue(file.name);
+      }
     }
+  }
+
+  onAllRiskChange(event: any) {
+    this.allRisk = event.target.value;
   }
 
   uploadFile(file: File): Promise<string> {
@@ -93,14 +146,24 @@ export class RegisterCompetitorComponent {
   }
 
   async onSubmit() {
-    if (this.form.valid) {
+    if (this.formVehicle.valid) {
+      //Actualizar el valor de allRisk
+      this.formVehicle.patchValue({ allRisk: this.allRisk });
       try {
-        if (this.selectedFile) {
-          const url = await this.uploadFile(this.selectedFile);
-          this.form.patchValue({ image: url });
+        if (this.selectedCompetitorFile) {
+          const competitorUrl = await this.uploadFile(
+            this.selectedCompetitorFile
+          );
+          this.formCompetitor.patchValue({ competitorImage: competitorUrl });
         }
-        console.log('Form Submitted!', this.form.value);
-        // Aquí puedes manejar el envío del formulario con la URL de la imagen
+        if (this.selectedVehicleFile) {
+          const vehicleUrl = await this.uploadFile(this.selectedVehicleFile);
+          this.formVehicle.patchValue({ vehicleImage: vehicleUrl });
+        }
+
+        this.competitor = this.formCompetitor.value;
+        this.competitor.vehicle = this.formVehicle.value;
+        console.log(this.competitor);
       } catch (error) {
         console.error('Error uploading file:', error);
       }
